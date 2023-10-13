2cc0c3080b1d38276ed987ef5f622ebeff4b153c
RemiLeBeau <remilebeau90@gmail.com>
2023-10-13 16:57:25 -0400

    changed socket url to render url

--- server/public/app.js
+++ server/public/app.js
@@ -1,4 +1,4 @@
-const socket = io("ws://localhost:3500");
+const socket = io("https://io-project-kqna.onrender.com");
 
 const msgInput = document.querySelector("#message");
 const nameInput = document.querySelector("#name");
